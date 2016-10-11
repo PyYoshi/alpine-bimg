07fa98039dd2f2dc0b72ea050a8ec43ef6f0b57e
PyYoshi <myoshi321go@gmail.com>
2016-10-12 00:25:24 +0900

    :yum: improve build script

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+export VERSION=1.7.1-8.4.1
+
 # docker images -q | xargs docker rmi -f
 
-docker build -t pyyoshi/alpine-bimg:latest .
+docker build -t pyyoshi/alpine-bimg:latest -t pyyoshi/alpine-bimg:$VERSION .
 # docker push pyyoshi/alpine-bimg:latest
 
 # docker run -i -t pyyoshi/alpine-bimg:latest /bin/sh
